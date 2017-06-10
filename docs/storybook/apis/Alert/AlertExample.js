a76788a44911557afca662ebf31c28430129eed4
vaukalak <lahmataja.pa4vara@gmail.com>
2017-06-10 22:19:15 +0300

    updated examples to support react-native-web new version

--- docs/storybook/apis/Alert/AlertExample.js
+++ docs/storybook/apis/Alert/AlertExample.js
@@ -1,5 +1,5 @@
-import { StyleSheet, Button, View, Alert } from 'react-native'
-import ReactNativeApp from '../../../../src/apis/AppRegistry/AppContainer'
+import { Button, View, Alert } from 'react-native'
+import AppContainer from '../../../../src/apis/AppRegistry/AppContainer'
 import React, { Component } from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 
@@ -9,76 +9,82 @@ const style = {
   },
 };
 
+const showAlert = (...props) => () => Alert.alert(...props);
+
 class AlertExample extends Component {
   render() {
     return (
-      <ReactNativeApp
+      <AppContainer
         rootTag="test"
-        rootComponent={() => (
-          <View>
-            <Button title="DEFAULT BUTTON"
-              onPress={
-                () => Alert.alert(
-                  'Hey There',
-                  'This is an example of how should we work with default button alert!'
-                )
-              }
-            />
-            <View style={style.spacer} />
-            <Button title="CUSTOM SINGLE BUTTON"
-              onPress={
-                () => Alert.alert(
-                  'Hey There',
-                  'This is an example of how should we work with custom single button alert!',
-                  [
-                    { text: 'Hey!', onPress: action('Hey!') }
-                  ]
-                )
-              }
-            />
-            <View style={style.spacer} />
-            <Button title="DOUBLE BUTTON"
-              onPress={
-                () => Alert.alert(
-                  'Hey There',
-                  'This is an example of how should we work with one button alert!',
-                  [
-                    { text: 'Yes!', onPress: action('Yes!') },
-                    { text: 'No!', onPress: action('No!') }
-                  ]
-                )
-              }
-            />
-            <View style={style.spacer} />
-            <Button title="MULTI BUTTON"
-              onPress={
-                () => Alert.alert(
-                  'Hey There',
-                  'This is an example of how should we work with one button alert!',
-                  [
-                    { text: 'One', onPress: action('One') },
-                    { text: 'Two', onPress: action('Two') },
-                    { text: 'Three', onPress: action('Three') }
-                  ]
-                )
-              }
-            />
-            <View style={style.spacer} />
-            <Button title="CANCELABLE"
-              onPress={
-                () => Alert.alert(
-                  'Hey There',
-                  'Tap background to close',
-                  [
-                    { text: 'One', onPress: action('One') },
-                  ],
-                  { cancelable: true }
-                )
-              }
-            />
-          </View>
-        )}
-      />
+      >
+        <View>
+          <Button
+            onPress={
+              showAlert(
+                'Hey There',
+                'This is an example of how should we work with default button alert!'
+              )
+            }
+            title="DEFAULT BUTTON"
+          />
+          <View style={style.spacer} />
+          <Button
+            onPress={
+              showAlert(
+                'Hey There',
+                'This is an example of how should we work with custom single button alert!',
+                [
+                  { text: 'Hey!', onPress: action('Hey!') }
+                ]
+              )
+            }
+            title="CUSTOM SINGLE BUTTON"
+          />
+          <View style={style.spacer} />
+          <Button
+            onPress={
+              showAlert(
+                'Hey There',
+                'This is an example of how should we work with one button alert!',
+                [
+                  { text: 'Yes!', onPress: action('Yes!') },
+                  { text: 'No!', onPress: action('No!') }
+                ]
+              )
+            }
+            title="DOUBLE BUTTON"
+          />
+          <View style={style.spacer} />
+          <Button
+            onPress={
+              showAlert(
+                'Hey There',
+                'This is an example of how should we work with one button alert!',
+                [
+                  { text: 'One', onPress: action('One') },
+                  { text: 'Two', onPress: action('Two') },
+                  { text: 'Three', onPress: action('Three') }
+                ]
+              )
+            }
+            title="MULTI BUTTON"
+          />
+          <View style={style.spacer} />
+          <Button
+            onPress={
+              showAlert(
+                'Hey There',
+                'Tap background to close',
+                [
+                  { text: 'One', onPress: action('One') },
+                ],
+                { cancelable: true }
+              )
+            }
+            title="CANCELABLE"
+          />
+        </View>
+      </AppContainer>
     );
   }
 }
